4850260c7005e493401ed12491aedf657ac3822c
AshDashTooFast3 <338127@student.mboutrecht.nl>
2025-11-28 10:38:20 +0100

    verwijzing bug gefixed

--- resources/views/praktijkmanagement/userroles.blade.php
+++ resources/views/praktijkmanagement/userroles.blade.php
@@ -19,14 +19,14 @@
                                     <button type="button" class="btn-close" data-bs-dismiss="alert"
                                         aria-label="Sluiten"></button>
                                 </div>
-                                <meta http-equiv="refresh" content="3;url={{ route('praktijkmanagement.index') }}">
+                                <meta http-equiv="refresh" content="3;url={{ route('praktijkmanagement.userroles') }}">
                             @elseif (session('error'))
                                 <div class="alert alert-danger alert-dismissible fade show" role="alert">
                                     {{ session('error') }}
                                     <button type="button" class="btn-close" data-bs-dismiss="alert"
                                         aria-label="Sluiten"></button>
                                 </div>
-                                <meta http-equiv="refresh" content="3;url={{ route('praktijkmanagement.index') }}">
+                                <meta http-equiv="refresh" content="3;url={{ route('praktijkmanagement.userroles') }}">
                             @endif
 
                             <table class="table table-striped table-bordered align-middle shadow-sm">
